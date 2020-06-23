37e741dee648e713db9ec5932f0f9ddc15891fad
Carlo Paredes <c.nick.paredes@gmail.com>
2020-06-23 16:22:28 -0700

    First commit

---
+++ Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class Routes extends React.Component { 
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Routes
---
+++ src/components/ScheduleEvent/ScheduleEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ScheduleEvent extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default ScheduleEvent
